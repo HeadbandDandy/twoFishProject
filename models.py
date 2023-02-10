ad1e46f561f08a521b0e37e1e007a8a3876f75c8
Darryl Johnson <dandycodes@gmail.com>
2023-02-09 19:02:21 -0500

    packages installed

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# app name below
 app = Flask(__name__)
+# database configuration below
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# App extension below
 db = SQLAlchemy(app)
 
 class User(db.Model):
