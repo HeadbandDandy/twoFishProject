a3da378ddc8057e0db69f0c1073c9e3064b766e4
Darryl Johnson <dandycodes@gmail.com>
2023-02-13 13:52:42 -0500

    login template set up

--- app/routes/home.py
+++ app/routes/home.py
@@ -2,8 +2,12 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
+# route for main page
 @bp.route('/')
 def index():
     return render_template('index.html')
 
-
+# route for login page
+@bp.route('/login')
+def login():
+    return render_template()
