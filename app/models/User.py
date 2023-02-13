3f586408e396d26d2477535155159ead84984e87
Darryl Johnson <dandycodes@gmail.com>
2023-02-13 12:45:02 -0500

    hashing and validation works

--- app/models/User.py
+++ app/models/User.py
@@ -1,9 +1,13 @@
+from typing import ValuesView
 from app.db import Base
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.orm import validates
+import bcrypt
+
+salt = bcrypt.gensalt()
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
@@ -17,4 +21,19 @@ class User(Base):
         assert '@' in email 
         
         return email 
+    
+    # validation for passwords
+    @validates('password')
+    def validate_password(self, key, password):
+        assert len(password) > 5
+        # below encrypts password
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    # below verifies the password
+    def verify_password(self, password):
+        return bcrypt.checkpw(
+            password.encode('utf-8'),
+            self.password.encode('utf-8')
+        )
+
 
