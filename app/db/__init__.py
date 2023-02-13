fc304ddffe83bafeb57b095c6ad408b314e1dc83
Darryl Johnson <dandycodes@gmail.com>
2023-02-13 09:05:57 -0500

    adjusted file structure added seeds file added filters file

---
+++ app/db/__init__.py
@@ -0,0 +1,29 @@
+from os import getenv
+from flask import g
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+engine = create_engine(getenv('DB_URL'), exho=True, pool_size=20, max_overflow=0)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+def init_db(app):
+    Base.metadata.create_all(engine) 
+    app.teardown_appcontext(close_db)
+    
+def get_db():
+    g.db = Session()
+    
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    
+    if db is none None:
+        db.close()
---
+++ app/utils/filters.py
@@ -0,0 +1,28 @@
+# below will be functions set for filtering through data
+def format_date(date):
+    return date.strftime('%m/%d/%y')
+
+from datetime import datetime 
+# print(format_date(datetime.now()))
+
+# below contains function to format and filter URL
+# below removes extra information from url leaving only domain name
+def format_url(url):
+    return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+
+
+# Test for above function
+# print(format_url('http://google.com/test/'))
+
+
+# below contains function to format plural words
+def format_plural_words(amount, word):
+    if amount != 1:
+        return word + 's'
+
+    return word
+
+# below tests for above function
+# print(format_plural_words(2, 'cat'))
+# print(format_plural_words(1, 'dog'))
+# print(format_plural_words(5, 'elephant'))
