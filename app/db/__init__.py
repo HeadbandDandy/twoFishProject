7787d5acb39f72726d0eb6607f9cf7c061996159
Darryl Johnson <dandycodes@gmail.com>
2023-02-17 20:52:24 -0500

    tired

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,9 +1,9 @@
 from os import getenv
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from sqlmodel import Field, SQLModel
 # from flask_sqlalchemy import SQLAlchemy
 
 
@@ -12,4 +12,7 @@ load_dotenv()
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+
+
 
