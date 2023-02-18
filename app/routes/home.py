a53da938752e8ac243f0fc5ef36728f8ce4102b2
Darryl Johnson <dandycodes@gmail.com>
2023-02-18 18:30:20 -0500

    server still loads fine doing well my guy

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, render_template
+from app.models import ToDo
+from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 # route for main page
 @bp.route('/')
 def index():
+    # get all todos
+    db = get_db()
+    todos = db.query(ToDo).order_by(ToDo.created_at.desc()).all()
     return render_template('index.html')
 
 # route for login page
