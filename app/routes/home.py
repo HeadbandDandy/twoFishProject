268c1d9263af04c2385a21301db81fdb1cdc34a7
Darryl Johnson <dandycodes@gmail.com>
2023-02-19 12:37:34 -0500

    models checked

--- app/routes/home.py
+++ app/routes/home.py
@@ -5,18 +5,21 @@ from app.db import get_db
 bp = Blueprint('home', __name__, url_prefix='/')
 
 # route for main page
-@bp.route('/')
+@bp.route('/homepage')
 def index():
     # get all todos
     db = get_db()
     todos = db.query(ToDo).order_by(ToDo.created_at.desc()).all()
-    return render_template('index.html')
+    return render_template('homepage.html')
 
 # route for login page
-@bp.route('/login')
-def login():
-    return render_template('login.html')
-
+@bp.route('/')
+def signin():
+    return render_template('signin.html')
+# route for signing up below
 @bp.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+    
